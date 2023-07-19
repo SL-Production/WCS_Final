50010a02cf352a1751d3bc0ce3b168604758f2a0
SL PRODUCTION <detections.eu@gmail.com>
2023-07-20 00:40:57 +0200

    near to good ui

--- frontend/src/pages/Home/MusicPlayer/Timeline/index.jsx
+++ frontend/src/pages/Home/MusicPlayer/Timeline/index.jsx
@@ -1,32 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import styles from "../index.module.scss";
 
-function Timeline({ currentTime, duration, onTimeUpdate }) {
-  const handleTimeUpdate = (event) => {
-    const { newCurrentTime } = event.target;
-    onTimeUpdate(newCurrentTime);
-  };
+function Timeline({ audio }) {
+  const progressBarRef = useRef(null);
+
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      const progress = (audio.currentTime / audio.duration) * 100;
+      if (progressBarRef.current) {
+        progressBarRef.current.style.width = `${progress}%`;
+      }
+    };
+
+    audio.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      audio.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, [audio]);
 
   return (
     <div className={styles.timeline}>
-      <input
-        type="range"
-        min={0}
-        max={duration}
-        value={currentTime}
-        step={1}
-        className={styles.progressSlider}
-        onChange={handleTimeUpdate}
-      />
+      <div className={styles.progressBar}>
+        <div ref={progressBarRef} className={styles.progressFilled} />
+      </div>
     </div>
   );
 }
 
 Timeline.propTypes = {
-  currentTime: PropTypes.number.isRequired,
-  duration: PropTypes.number.isRequired,
-  onTimeUpdate: PropTypes.func.isRequired,
+  audio: PropTypes.instanceOf(Audio).isRequired,
 };
 
 export default Timeline;
